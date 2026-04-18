5b7c91e527c1785c8d2832fa844b6b5a1a8e728c
razannnnnn <razanofc@gmail.com>
2026-04-18 23:24:47 +0700

    fix bug 1

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,15 @@
 import { Geist } from "next/font/google";
 import "./globals.css";
-import Providers from "./providers";
+import { Providers } from "./providers";
 
 const geist = Geist({ subsets: ["latin"] });
 
 export const metadata = {
   title: "RumahFilm",
   description: "Media server lokal pribadi",
+  icons: {
+    icon: "@/app/favicon.svg",
+  }
 };
 
 export default function RootLayout({ children }) {
